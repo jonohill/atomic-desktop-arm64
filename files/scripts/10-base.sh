393b6069e8500bb831dda781987e5ea5af925d21
Alex Iribarren <alex.iribarren@gmail.com>
2025-08-03 18:23:20 +0200

    Get EPEL's epel-release

--- files/scripts/10-base.sh
+++ files/scripts/10-base.sh
@@ -5,4 +5,7 @@ set -xeuo pipefail
 dnf install -y 'dnf-command(config-manager)' epel-release
 dnf config-manager --set-enabled crb
 
+# EPEL ships it's own epel-release package, let's make sure we've got that one
+dnf upgrade -y epel-release
+
 dnf install -y system-reinstall-bootc
